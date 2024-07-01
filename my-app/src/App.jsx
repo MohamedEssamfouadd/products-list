b9065921a790f2a2eb285bc38811c955a0a4e77e
USER <mohamedmezoo210443@gmail.com>
2024-07-01 14:54:22 +0300

    last commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { Header } from "./components/header/Header";
+import Card from "./components/card/Card";
+import { axiosConfig } from "./assets/axios/axiosConfig";
+import Pagination from "./components/pagination/Pagination";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [getLimit, setGetLimit] = useState(5);
+  async function getAllProduct(limit) {
+    const { data } = await axiosConfig({
+      url: `/products?limit=${limit}`,
+      method: "GET",
+    });
+    setProducts(data);
+  
+  }
+  useEffect(() => {
+    getAllProduct(getLimit);
+  }, []);
+  useEffect(() => {
+    getAllProduct(getLimit);
+  }, [getLimit]);
+
+  return (
+    <div className="app--container">
+      <Header />
+      <div className="pagination--header">
+        <span>{products.length} Items</span>
+        <Pagination setGetLimit={setGetLimit} getLimit={getLimit} />
+      </div>
+      <div className="card--container">
+        {products.map((data, index) => (
+          <Card key={index} data={data} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ my-app/src/components/pagination/Pagination.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Pagination = ({ setGetLimit, getLimit }) => {
+  return (
+    <div className="pagination">
+      <div className={`Pagination--item ${getLimit == 5 ? "active" : ""}`} onClick={(e) => setGetLimit(Number(e.target.textContent))}>
+        5
+      </div>
+      <div className={`Pagination--item ${getLimit == 10 ? "active" : ""}`} onClick={(e) => setGetLimit(Number(e.target.textContent))}>
+        10
+      </div>
+      <div className={`Pagination--item ${getLimit == 15 ? "active" : ""}`} onClick={(e) => setGetLimit(Number(e.target.textContent))}>
+        15
+      </div>
+      <div className={`Pagination--item ${getLimit == 20 ? "active" : ""}`} onClick={(e) => setGetLimit(Number(e.target.textContent))}>
+        20
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
